19172a0afe4db27dedd0fa9702a4b1bec2f23acc
Bot zin <samchannboth@gmail.com>
2023-12-29 14:31:10 +0700

    [Bot] add message and navigate

--- hr-management-fe/pages/login/index.vue
+++ hr-management-fe/pages/login/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex items-center justify-center h-screen">
     <n-space justify="center" align="center" vertical inline>
+      <n-message-provider>
+        <content />
+      </n-message-provider>
       <n-input
         v-model:value="value"
         type="text"
@@ -18,6 +21,10 @@
         round
         style="width: 250px"
       />
+      <n-button strong secondary round type ="success"  @click="HandleClick" >
+        Success
+      </n-button>
+      
     </n-space>
   </div>
 </template>
@@ -25,7 +32,9 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { GlassesOutline, Glasses } from "@vicons/ionicons5";
-import { NSpace, NInput, NIcon, darkTheme } from "naive-ui";
+import { NSpace, NInput, NIcon, darkTheme  } from "naive-ui";
+import {useMessage} from 'naive-ui';
+import { LoginFnc} from "./method";
 
 export default defineComponent({
   setup() {
@@ -34,7 +43,17 @@ export default defineComponent({
       Glasses,
       value: ref(null),
       darkTheme,
+      LoginFnc,
+      message,
+      HandleClick
     };
+
   },
 });
+const message = useMessage();
+            const HandleClick =() => {
+                message.warning('Login Success');
+            };
+
 </script>
+
