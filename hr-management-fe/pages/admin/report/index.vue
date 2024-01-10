6ed144e9c1cef1d278f66d2db549ee35def6fc7f
Bot zin <samchannboth@gmail.com>
2024-01-11 00:09:10 +0700

    [FE] add new report page

---
+++ hr-management-fe/pages/admin/report/index.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <h1>Report</h1>
+  <n-layout style="flex-grow: 1; display: flex; flex-direction: column">
+    <n-card
+      content-style="padding: 10px;"
+      style="height: 50px; overflow: hidden"
+    >
+      <div
+        style="
+          flex-direction: row;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          overflow: hidden;
+        "
+      >
+        <n-text type="primary" style="font-size: 18px">Report</n-text>
+      </div></n-card
+    >
+  </n-layout>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout:"main"
+})
+</script>
